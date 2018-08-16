9fb3cb60d9efdcaa742811307150ecdddb077638
Freddy Contreras <freddycontreras3@gmail.com>
2018-08-15 20:51:57 -0500

    Test Lazy Load

--- src/shared/Card/components/Card-Image.js
+++ src/shared/Card/components/Card-Image.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import injectSheet from 'react-jss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import LazyLoad from 'react-lazyload';
+
 const styles = {
   cardImage: {
     '&:hover': {
@@ -29,7 +31,9 @@ const styles = {
 const CardImage = props => (
   <div className={props.classes.cardImage}>
     <FontAwesomeIcon icon='search' className={props.classes.icon}/>
-    <img src={props.image} alt={props.title} className={props.classes.image}/>
+    <LazyLoad >
+      <img src={props.image} alt={props.title} className={props.classes.image}/>
+    </LazyLoad>
   </div>
 )
 
