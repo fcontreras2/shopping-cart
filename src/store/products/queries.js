96aad12b24628fdacb2eb88ba5e29d5a0e3f7d1f
Freddy Contreras <freddycontreras3@gmail.com>
2018-08-17 14:09:22 -0500

    URL de prod y desarrollo

--- src/store/products/queries.js
+++ src/store/products/queries.js
@@ -1,12 +1,13 @@
 const withQuery = require('with-query').default;
-
+const API_URL = process.env.REACT_APP_ENV === 'prod' ? 
+  "https://shopping-cart-data.herokuapp.com/" : 
+  "http://localhost:4000/"
 
 export const queryData = () => (
-  fetch('https://shopping-cart-data.herokuapp.com/categories/')
+  fetch(API_URL + 'categories/')
     .then(resp => resp.json())
 )
-
-
+  
 export const querySearch = query => (
-  fetch(withQuery('http://localhost:4000/search',query)).then(resp => resp.json())
+  fetch(withQuery(API_URL + 'search',query)).then(resp => resp.json())
 )
